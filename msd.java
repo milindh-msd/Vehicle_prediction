42833186078565d954455c67358b90d62fc75416
milindh-msd <deshapathimilindhsharma13.2005@gmail.com>
2025-11-17 20:51:26 +0530

    Update msd.java

--- msd.java
+++ msd.java
@@ -1,4 +1,6 @@
-public class{
-public static void main(string[] args){
-System.out.println("hello");
-}}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
